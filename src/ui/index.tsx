5403702f9d60d2c4c137532f98013069eca2869d
Luke Berry <boondaburrah@airedale.home>
2018-03-05 00:43:03 -0500

    beginning layout

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
+import { Layout } from "antd";
 
 export default class MainWindow extends React.Component {
     public render(){
-        return (<div>Hello!</div>);
+        return <Layout>
+            <Layout.Sider>
+                <div>Sidebar</div>
+            </Layout.Sider>
+            <Layout.Content>
+                <div>Content</div>
+            </Layout.Content>
+        </Layout>;
     }
 }
