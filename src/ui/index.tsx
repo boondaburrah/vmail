8eef8c17c73c63da68f71d0c988a967eea0720cc
Luke Berry <boondaburrah@airedale.home>
2018-03-05 10:21:53 -0500

    fake mailbox for mockup

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,18 +1,29 @@
 import * as React from "react";
 import { Layout, Menu } from "antd";
+import Mailbox from "./mailbox";
+import { sampleMail } from "./mailbox";
 
 export default class MainWindow extends React.Component {
     public render(){
         return <Layout>
             <Layout.Sider>
                 <Menu theme="dark" mode="inline">
-                    <Menu.Item key="1">
-                        Menu Item One!
+                    <Menu.Item key="inbox">
+                        Inbox
+                    </Menu.Item>
+                    <Menu.Item key="starbox">
+                        Starred
+                    </Menu.Item>
+                    <Menu.Item key="sentbox">
+                        Sent Mail
+                    </Menu.Item>
+                    <Menu.Item key="draftsbox">
+                        Drafts
                     </Menu.Item>
                 </Menu>
             </Layout.Sider>
             <Layout.Content>
-                <div>Content</div>
+                <Mailbox currentPage={1} mailboxName="Inbox" mailItems={sampleMail}/>
             </Layout.Content>
         </Layout>;
     }
---
+++ src/ui/mailbox.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import * as R from "ramda";
+import { Table } from "antd";
+
+type mailItemList = any[];
+
+interface MailboxProperties {
+    currentPage: number;
+    mailboxName: string;
+    mailItems: mailItemList;
+}
+
+export const sampleMail: mailItemList = R.map((i) => {
+    return {
+        address: "example".concat(Number(i).toFixed(0), "@example.net"),
+        subject: "Fake Subject Number ".concat(Number(i).toFixed(0)),
+        timestamp: Date.now().toString()
+    };
+}, R.range(0, 10));
+
+const mailboxColumns = [
+  { title: "Address",
+    dataIndex: "address",
+    key: "address" },
+  { title: "Subject",
+    dataIndex: "subject",
+    key: "subject" },
+  { title: "Date",
+    dataIndex: "timestamp",
+    key: "timestamp" }
+];
+
+export default function Mailbox(props: MailboxProperties){
+    return <Table dataSource={props.mailItems} columns={mailboxColumns}>
+
+    </Table>;
+}
