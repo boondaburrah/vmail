e78a7ee2ee296501e5640bb81d2eb37bc5f7545b
Luke Berry <luke@braid.com>
2018-03-10 12:52:01 -0500

    compiles now. Work on your promises

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,4 @@
-import D from "dexie";
-import A from "axios";
+import A, {AxiosResponse} from "axios";
 
 export interface MailItem {
     id: number;
@@ -12,42 +11,28 @@ export interface MailItem {
 }
 
 export default class JeeCore{
-    private jeedb: D;
+    private jeedb: MailItem[];
 
     constructor(){
-        this.jeedb = new D("jeemail");
-        this.jeedb.version(0).stores({
-            mailItem: "id,subject,body,from,to,date,mailbox"
-        });
-
-        this.jeedb.on("ready", this.dbIsReady);
+        this.jeedb = [{
+            id: 0,
+            subject: "No Email Data",
+            body: "AJAX hasn't come back with the email data.",
+            from: "jee@mail.itself",
+            to: "you@dear.reader",
+            date: 0,
+            mailbox: "inbox"
+        }];
+        A.get("/emails.json").then(this.initDataFromJson);
     }
 
-    private dbIsReady(){
-        // check if DB has already been loaded. If not, fetch the json by ajax and load it up
-        this.jeedb.table("mailItem").count().then((c) => {
-            if (c > 0){
-                // table already has stuff in it. Do nothing.
-                console.log("database found already with data");
-                return;
-            }
-            // If we've gotten to this point, that means the mailItem table is empty.
-            console.log("mailItem DB is empty. We should put something in it. I'll try to fetch /emails.json now.");
-            return A.get("/emails.json").then((response) => {
-                console.log("Ajax came back, got a response code: ".concat(response.status.toFixed(0)));
-                if(response.status !== 200){
-                    console.error("that's not good. Error text was: ".concat(response.statusText));
-                    console.log("Bailing on AJAX error");
-                    return;
-                }
-                this.populateEmailTable(response.data);
-            });
-        });
-    }
-
-    private populateEmailTable(data: MailItem[]){
-        if(this.jeedb.isOpen()){
-            //pass
+    private initDataFromJson(response: AxiosResponse){
+        if(response.status === 200){
+            console.log("DATA GET");
+            this.jeedb = response.data;
+        } else {
+            console.log("DATA NOT GET: ".concat(response.status.toFixed(0)).concat(response.statusText));
+            debugger;
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import * as rdom from "react-dom";
 import {Provider, ProviderProps} from "react-redux";
 import MainWindow from "./ui";
 import State from "./state";
+import Core from "./core";
+
+const core = new Core();
 
 rdom.render(React.createElement(Provider, {store: State as any},
     React.createElement(MainWindow)),
